6485ff53bbbf2b482ad2d9c6638ca2227ec532c9
Noa <30934548+noanor@users.noreply.github.com>
2025-06-09 10:19:01 +0200

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
                 <img 
                   src="../public/Logo Upscaled.png" 
                   alt="Agoras Logo" 
-                  className="w-6 h-6 object-contain filter brightness-1"
+                  className="w-6 h-6 object-contain filter brightness-1 rounded-lg"
                 />
               </div>
               <div>
